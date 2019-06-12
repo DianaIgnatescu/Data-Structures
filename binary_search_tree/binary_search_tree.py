d9d6665a8dc79133dbb8521d7149f7d6c80344cf
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-12 13:21:13 +0100

    Implement insert method for BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,11 +1,26 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # Check new node val  < current nodes val
+        if value < self.value:
+            # if no left child place new node
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+            # otherwise repeat process
+                self.left.insert(value)
+            # check new node val >= current nodes val
+        if value >= self.value:
+            # if no right child place new node
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            # otherwise repeat process
+            else:
+                self.right.insert(value)
 
   def contains(self, target):
     pass
