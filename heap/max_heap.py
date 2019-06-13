3cd5d7fd8ba998fe26ce330c34a3d701ffbd62ff
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-13 18:13:31 +0100

    Implement sift_down and delete for Heap & check that tests pass

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,7 +7,11 @@ class Heap:
         return self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        deleted = self.storage[0]
+        self.storage[0] = self.storage[-1]
+        self.storage.pop()
+        self._sift_down(0)
+        return deleted
 
     def get_max(self):
         return self.storage[0]
@@ -35,4 +39,18 @@ class Heap:
                 break
 
     def _sift_down(self, index):
-        pass
+        last_index = len(self.storage) - 1
+        while index * 2 + 1 <= last_index:
+            left_child_index = index * 2 + 1
+            right_child_index = index * 2 + 2
+            if right_child_index > last_index:
+                largest_index = left_child_index
+            else:
+                if self.storage[left_child_index] > self.storage[right_child_index]:
+                    largest_index = left_child_index
+                else:
+                    largest_index = right_child_index
+
+            if self.storage[index] < self.storage[largest_index]:
+                self.storage[index], self.storage[largest_index] = self.storage[largest_index], self.storage[index]
+            index = largest_index
